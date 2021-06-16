93a7756043abb2684eb73bcd3449f32113718d6c
Dheepak Krishnamurthy <me@kdheepak.com>
2021-06-15 22:13:24 -0600

    Refactor

--- frontend/src/components/Tools.vue
+++ frontend/src/components/Tools.vue
@@ -13,122 +13,22 @@
       :data="[]"
     />
 
-    <div id="toolbar">
-      <button
-        v-on:click="reset"
-        id="button"
-        class="btn btn-secondary"
-        style="margin-left: 1em"
-      >
-        <i class="fa fa-sync"></i>
-      </button>
-    </div>
-    <BootstrapTable :columns="columns" :data="toolsQuery" :options="options" />
-
     <br />
 
-    <div class="row" v-if="!toolsLoaded">
-      <div class="col">
-        <img src="/images/loading.svg" alt="" />
-      </div>
-    </div>
+    <ToolsTable />
   </div>
 </template>
 
 <script lang="ts">
 import { ref, defineComponent } from "vue";
-import axios from "axios";
 import Autocomplete from "./Autocomplete.vue";
-
-import { mapState, mapGetters, mapActions } from "vuex";
+import ToolsTable from "./ToolsTable.vue";
 
 export default defineComponent({
   name: "Tools",
   components: {
     Autocomplete,
-  },
-  mounted() {
-    this.getLocalData();
-  },
-  data() {
-    return {
-      options: {
-        search: false,
-        showColumns: true,
-        buttonsAlign: "right",
-        showPaginationSwitch: true,
-        showExtendedPagination: true,
-        pagination: true,
-        sortReset: true,
-        sortName: "name_url",
-        sortOrder: "asc",
-        toolbar: "#toolbar",
-        toolbarAlign: "right",
-      },
-      repository: "Tools",
-      response: {
-        status: "",
-        message: "",
-      },
-    };
-  },
-  computed: {
-    columns() {
-      return [
-        {
-          field: "state",
-          checkbox: true,
-        },
-        {
-          field: "name_url",
-          title: "Name",
-          align: "left",
-          halign: "left",
-          sortable: true,
-          sortName: "name",
-        },
-        {
-          field: "short_description",
-          title: "Description",
-          align: "left",
-          halign: "left",
-        },
-        {
-          field: "source_url",
-          title: "Source",
-        },
-        {
-          field: "github_stars",
-          title: "Stars",
-          sortable: true,
-        },
-      ];
-    },
-    showTools() {
-      return this.tools.length > 0 && this.toolsLoaded;
-    },
-    noTools() {
-      return this.toolsQuery.length === 0 && this.toolsLoaded;
-    },
-    ...mapGetters(["tools", "toolsQuery", "toolsLoaded"]),
-  },
-  methods: {
-    reset() {
-      this.clearTools();
-      this.getTools();
-    },
-    resetResponse() {
-      this.response.status = "";
-      this.response.message = "";
-    },
-    getTools() {
-      this.resetResponse();
-      this.fetchTools();
-    },
-    getLocalData() {
-      this.getTools();
-    },
-    ...mapActions(["fetchTools", "clearTools"]),
+    ToolsTable,
   },
 });
 </script>
---
+++ frontend/src/components/ToolsTable.vue
@@ -0,0 +1,133 @@
+<template>
+  <div id="toolbar">
+    <button
+      v-on:click="reset"
+      id="button"
+      class="btn btn-secondary"
+      style="margin-left: 1em"
+    >
+      <i class="fa fa-sync"></i>
+    </button>
+  </div>
+
+  <BootstrapTable :columns="columns" :data="toolsQuery" :options="options" />
+
+  <br />
+
+  <div class="row" v-if="!toolsLoaded">
+    <div class="col">
+      <img src="/images/loading.svg" alt="" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+import axios from "axios";
+import { mapState, mapGetters, mapActions } from "vuex";
+
+export default defineComponent({
+  name: "ToolsTable",
+  mounted() {
+    this.getLocalData();
+  },
+  data() {
+    return {
+      options: {
+        search: false,
+        showColumns: true,
+        buttonsAlign: "right",
+        showPaginationSwitch: true,
+        showExtendedPagination: true,
+        pagination: true,
+        sortReset: true,
+        sortName: "name_url",
+        sortOrder: "asc",
+        toolbar: "#toolbar",
+        toolbarAlign: "right",
+      },
+      repository: "Tools",
+      response: {
+        status: "",
+        message: "",
+      },
+    };
+  },
+  computed: {
+    columns() {
+      return [
+        {
+          field: "state",
+          checkbox: true,
+        },
+        {
+          field: "name_url",
+          title: "Name",
+          align: "left",
+          halign: "left",
+          sortable: true,
+          sortName: "name",
+        },
+        {
+          field: "short_description",
+          title: "Description",
+          align: "left",
+          halign: "left",
+        },
+        {
+          field: "source_url",
+          title: "Source",
+        },
+        {
+          field: "github_stars",
+          title: "Stars",
+          sortable: true,
+        },
+      ];
+    },
+    showTools() {
+      return this.tools.length > 0 && this.toolsLoaded;
+    },
+    noTools() {
+      return this.toolsQuery.length === 0 && this.toolsLoaded;
+    },
+    ...mapGetters(["tools", "toolsQuery", "toolsLoaded"]),
+  },
+  methods: {
+    reset() {
+      this.clearTools();
+      this.getTools();
+    },
+    resetResponse() {
+      this.response.status = "";
+      this.response.message = "";
+    },
+    getTools() {
+      this.resetResponse();
+      this.fetchTools();
+    },
+    getLocalData() {
+      this.getTools();
+    },
+    ...mapActions(["fetchTools", "clearTools"]),
+  },
+});
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
