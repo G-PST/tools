de2a5cf8449e91b3c936ff16489405ed597d63b9
Dheepak Krishnamurthy <me@kdheepak.com>
2021-06-17 08:23:29 -0600

    Change split to split by words

--- frontend/src/store/modules/tools.js
+++ frontend/src/store/modules/tools.js
@@ -30,7 +30,7 @@ const getters = {
       tools = state.tools.filter((item) =>
         state.searchQuery
           .toLowerCase()
-          .split(/[-./\\()"',;<>~!@#$%^&*|+=[\]{}`~?:]/u)
+          .split(/[-./\\()"'\s,;<>~!@#$%^&*|+=[\]{}`~?:]/u)
           .every((v) => {
             return (
               item.name.toLowerCase().includes(v) ||
