1a8ec4f6c7e0b08ac0655a6ec95728606a7a9e75
Dheepak Krishnamurthy <me@kdheepak.com>
2021-09-10 14:16:09 -0600

    Change about page

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,24 +1,13 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="https://globalpst.org/"
+      <a target="_blank" class="navbar-brand" href="https://globalpst.org/"
         ><img
           src="/images/GPSTC-logo-blk-x2.png"
           width="181"
           height="90"
           style="max-height: 90px; height: auto"
       /></a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
@@ -28,7 +17,12 @@
             >
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" to="/About">About</router-link>
+            <a
+              target="_blank"
+              class="nav-link"
+              href="https://globalpst.org/what-we-do/open-data-tools/"
+              >About</a
+            >
           </li>
           <li class="nav-item">
             <a
