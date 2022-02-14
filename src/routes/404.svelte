3e2a6f964b1eec428fee9a83a87cb78198c430ec
Dheepak Krishnamurthy <me@kdheepak.com>
2022-02-14 12:41:35 -0700

    chore: Update files ♻️

--- src/routes/404.svelte
+++ src/routes/404.svelte
@@ -8,12 +8,14 @@
   <title>G-PST Tools Portal</title>
 </svelte:head>
 
-<div class="grid-flow-row w-full items-stretch">
-  <div
-    class="bg-red-100 rounded-lg py-5 px-6 mb-3 text-base text-red-700 inline-flex items-center w-full"
-    role="alert"
-  >
-    <Icon class="w-4 h-4 mr-2 fill-current" data={faExclamationCircle} />
-    Something went wrong. Please contact the developers.
+<div class="grid mx-40 my-12">
+  <div class="grid-flow-row w-full items-stretch">
+    <div
+      class="bg-red-100 rounded-lg py-5 px-6 mb-3 text-base text-red-700 inline-flex items-center w-full"
+      role="alert"
+    >
+      <Icon class="w-4 h-4 mr-2 fill-current" data={faExclamationCircle} />
+      Something went wrong. Please contact the developers.
+    </div>
   </div>
 </div>
