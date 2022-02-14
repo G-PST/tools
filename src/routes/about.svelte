20a0c76913dbaa2a626c6e7c0003ceab6d0e16da
Dheepak Krishnamurthy <me@kdheepak.com>
2022-02-14 12:51:05 -0700

    chore: Update files ♻️

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -3,9 +3,11 @@
 </svelte:head>
 
 <div class="grid mx-20 mt-6">
-  <h1 class="text-4xl font-medium leading-tight mt-0 mb-2">About modeling tools</h1>
+  <h1 class="text-4xl font-medium leading-tight mt-0 mb-2">About the modeling tools website</h1>
 
   <p class="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
-    This work is funded by GPST
+    This work is funded by GPST. All the data and source code is open source and is available on <a
+      href="https://github.com/g-pst/tools">https://github.com/g-pst/tools</a
+    >. If you have any additional questions, feel free to contact the developers.
   </p>
 </div>
